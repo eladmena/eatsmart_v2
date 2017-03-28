8173cf4a939388fc87c252a8ce3534a9523d1d97
htg1ue <adiabramo@gmail.com>
2017-03-28 15:30:04 +0300

    update from json - edit rest

--- app/src/main/java/org/hackathon/eatsmart/EditRestaurantActivity.java
+++ app/src/main/java/org/hackathon/eatsmart/EditRestaurantActivity.java
@@ -10,6 +10,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class EditRestaurantActivity extends AppCompatActivity {
 
@@ -19,8 +23,7 @@ public class EditRestaurantActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_restaurant);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        initRestData();
 
         initRestaurantTypes();
         Button fab = (Button) findViewById(R.id.applyBtn);
@@ -36,5 +39,43 @@ public class EditRestaurantActivity extends AppCompatActivity {
         Spinner mySpinner = (Spinner) findViewById(R.id.restTypes);
 
         mySpinner.setAdapter(new ArrayAdapter<eRestaurantType>(this, android.R.layout.simple_spinner_item, eRestaurantType.values()));
+        mySpinner.setSelection(3, true);
+    }
+
+    private void initRestData()
+    {
+        JSONObject rest = Storage.getInstance().getLandverRest();
+        try {
+            SetText((TextView)findViewById(R.id.cityRestName),rest.getString("city"));
+            SetText((TextView)findViewById(R.id.restName),rest.getString("name"));
+            SetText((TextView)findViewById(R.id.streetNumberRestName),rest.getString("street_num"));
+            SetText((TextView)findViewById(R.id.streetRestName),rest.getString("street"));
+            setSpinnerSelection(rest.getString("type"));
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void SetText(TextView textView, String str)
+    {
+        int start = Math.max(textView.getSelectionStart(), 0);
+        int end = Math.max(textView.getSelectionEnd(), 0);
+        textView.setText(str.toCharArray(),0, str.length());
+    }
+
+    private void setSpinnerSelection(String selected)
+    {
+        int i = 0;
+        Spinner mySpinner = (Spinner) findViewById(R.id.restTypes);
+
+        for (eRestaurantType rest : eRestaurantType.values()) {
+            if(rest.toString().equals(selected))
+            {
+                mySpinner.setAdapter(new ArrayAdapter<eRestaurantType>(this, android.R.layout.simple_spinner_item, eRestaurantType.values()));
+                mySpinner.setSelection(i, true);
+                break;
+            }
+            i++;
+        }
     }
 }
