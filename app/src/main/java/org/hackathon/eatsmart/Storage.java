a17fa924cc2d28b06f6bff7db46cd09a125a5a79
Shay Tsadok <shayts7@gmail.com>
2017-03-29 12:30:40 +0300

    remove unused line

--- app/src/main/java/org/hackathon/eatsmart/Storage.java
+++ app/src/main/java/org/hackathon/eatsmart/Storage.java
@@ -70,7 +70,6 @@ public class Storage {
     private Storage() {
         try {
             jObj = new JSONObject(loadJsonFromResources());
-            //jObj = new JSONObject(json);
         } catch (JSONException e) {
             Log.e("JSON Parser", "Error parsing data " + e.toString());
         }
