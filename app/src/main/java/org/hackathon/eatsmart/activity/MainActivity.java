9e955bfd57e6f6a7635c85352b255dc7e6f04361
avisht <avi@shteiman.net>
2017-03-29 10:42:59 +0300

    Remove RestaurantMainActivity

--- app/src/main/java/org/hackathon/eatsmart/activity/MainActivity.java
+++ app/src/main/java/org/hackathon/eatsmart/activity/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         restaurantButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent menuIntent = new Intent(getApplicationContext(), RestaurantMainActivity.class);
+                Intent menuIntent = new Intent(getApplicationContext(), EditRestaurantActivity.class);
                 startActivity(menuIntent);
             }
         });
